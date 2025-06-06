c113ec4cbd2081e6360a553af67b2b752520c3da
sarforajs <128588277+sarforajs@users.noreply.github.com>
2025-06-06 22:04:56 +0530

    Update OrderController.java

---
+++ src/main/java/com/sarforaj/orderservice/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.sarforaj.orderservice.controller;
+
+import com.sarforaj.orderservice.model.Order;
+import com.sarforaj.orderservice.service.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.createOrder(order);
+    }
+}
