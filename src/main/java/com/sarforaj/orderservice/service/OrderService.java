4f9b7597c09a8281b617af8983df3fad324bef54
sarforajs <128588277+sarforajs@users.noreply.github.com>
2025-06-06 22:06:13 +0530

    Update OrderService.java

---
+++ src/main/java/com/sarforaj/orderservice/service/OrderService.java
@@ -0,0 +1,25 @@
+package com.sarforaj.orderservice.service;
+
+import com.sarforaj.orderservice.model.Order;
+import com.sarforaj.orderservice.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+}
